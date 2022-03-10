9cf9be61bc50406b852e82824aa67c25e4c8569b
alonsofdez23 <alonsobs23@gmail.com>
2022-03-10 12:58:00 +0100

    Path del SVG "-" del carrito añadido

--- resources/views/carritos/index.blade.php
+++ resources/views/carritos/index.blade.php
@@ -46,7 +46,7 @@
                                             @method('POST')
                                             <button type="submit">
                                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4v16m8-8H4" />
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 4 m 8 8 H 4" />
                                                 </svg>
                                             </button>
                                         </form>
